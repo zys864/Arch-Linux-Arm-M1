b242dd17993f3813c88d73538c22dc1ed8f0808a
zys864 <zyspage@gmail.com>
2023-04-27 22:00:59 +0800

    update by pacman

--- setupefi.sh
+++ setupefi.sh
@@ -4,6 +4,7 @@ EXT4_UUID=$(blkid -s UUID -o value /dev/vda2)
 rm /var/lib/pacman/db.lck # remove pacman lockfile?
 pacman-key --init
 pacman-key --populate archlinuxarm
+pacman -Syyu --noconfirm
 pacman -Syu --noconfirm efibootmgr
 efibootmgr --disk /dev/vda --part 1 --create --label "Arch Linux ARM" --loader /Image --unicode "root=UUID=$EXT4_UUID rw initrd=\initramfs-linux.img" --verbose
 rm $0
